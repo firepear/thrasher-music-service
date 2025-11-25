f6a496e5372cfe18fc60debb9b469219881c8007
Shawn Boyette <shawn@firepear.net>
2025-11-25 15:04:56 -0700

    mobile layout improvements for pwa

--- client/mobile.js
+++ client/mobile.js
@@ -17,13 +17,13 @@ function setOLandscape() {
     els.tracklist.style.display = "none";
     els.tracklist.style.height = "0vh";
     els.tracklist.style.marginBottom = "0";
-    els.player.style.height = "40vh";
+    els.player.style.height = "50vh";
 }
 
 function setOPortrait() {
     els.player.style.height = "20vh";
     els.tracklist.style.display = "block";
-    els.tracklist.style.height = "54vh";
+    els.tracklist.style.height = "60vh";
     els.tracklist.style.marginBottom = "0.5em";
 }
 

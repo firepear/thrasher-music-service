cee2580a12207eccdd8da4e052c0643600da39ac
Shawn Boyette <shawn@firepear.net>
2025-11-26 09:10:52 -0700

    version bump

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 // terser client.js --compress --mangle > client.js.min
-var tag = "v0.7.1";
+var tag = "v0.8.0";
 var proto = window.location.protocol;
 var host = window.location.host;
 var port = window.location.port;
